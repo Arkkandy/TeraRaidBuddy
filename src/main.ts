3e90413a3aa88188fc101d2bd0728e1b85b14589
Arkkandy <andresil.lg@gmail.com>
2024-07-11 17:24:14 +0100

    Pikachu 7 Star Re

--- src/main.ts
+++ src/main.ts
@@ -975,7 +975,7 @@ UIElements.RaidBoss.ExportImportButton.addEventListener( 'click', async () => {
 // ========================================================================
 // Set UI initial state (see index.html also)
 
-UIElements.RaidBoss.R7ESelect.value = "Sceptile";
+UIElements.RaidBoss.R7ESelect.value = "Pikachu";
 onPresetChange( RaidPresetMode.SevenStarEvent );
 
 
